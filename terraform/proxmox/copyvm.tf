bd148f33bf87818a30302e94469c0091ca56a21e
Dragroth <werander.k@gmail.com>
2024-03-06 14:05:13 +0000

    updateed tf provider version

---
+++ terraform/proxmox/copyvm.tf
@@ -0,0 +1,57 @@
+resource "proxmox_virtual_environment_vm" "name" {
+	name		= "name"
+	description = "Description"
+	tags 		= ["debian", "terraform"]
+
+	node_name	= "node"
+	vm_id		= 502
+
+	agent {
+		enabled = true
+	}
+
+	clone {
+		datastore_id = "local-lvm"
+		retries		= 3
+		vm_id		= 8010
+		full		= true
+	}
+
+	cpu {
+	  cores = 2
+	}
+
+	memory {
+	  dedicated = 1024
+	}
+
+	network_device {
+    	bridge = "vmbr100"
+  	}
+
+	disk {
+    	datastore_id	= "local-lvm"
+    	interface		= "scsi0"
+		size			= "8"
+  	}
+
+	vga {
+		enabled	= true
+		memory	= 16
+	}
+
+	# Cloud-init
+	initialization {
+		ip_config {
+			ipv4 {
+				address = "0.0.0.0/0"
+				gateway = "gateway"
+			}
+    	}
+
+		user_account {
+			username = "debian"
+			keys	 = ["key", "key2"]
+		}
+	}
+}
---
+++ terraform/proxmox/credentials.auto.tfvars
@@ -0,0 +1,2 @@
+proxmox_api_url             = "https://your-name:8006"
+proxmox_api_token_secret    = "root@pam!terraform=your-key"
